3dcb12641a7f459d09678d668cb11be77e8e4e04
unknown <saroball3@gmail.com>
2023-09-28 00:48:23 +0900

    Fix: Postgres Version:

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,16 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'database.cbexcccq21ba.ap-northeast-2.rds.amazonaws.com',
+    host: 'database-1.cbexcccq21ba.ap-northeast-2.rds.amazonaws.com',
     port: 5432,
     username: 'postgres',
     password: 'gkdnr8785',
     database: 'database1',
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: true, // Only for develnpmopment. Set to false in production.
-    ssl: {
-        "rejectUnauthorized": false
-      }
-};
+}
 
 
