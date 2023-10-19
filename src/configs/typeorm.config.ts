a50d0686efe9fce77953a35330d88e89c3eb094d
unknown <saroball3@gmail.com>
2023-10-20 04:18:10 +0900

    Fix: typeorm for deployment

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config';
 
 export const typeORMConfig = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
   type: 'postgres',
-  host:"localhost",
+  host: configService.get<string>('DATABASE_HOST'),
   port: configService.get<number>('DATABASE_PORT'),
   username: configService.get<string>('DATABASE_USERNAME'),
   password: configService.get<string>('DATABASE_PASSWORD'),
